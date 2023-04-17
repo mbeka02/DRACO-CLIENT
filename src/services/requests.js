f8b56e9f2def968d2839333a6bf557b0ea652569
mbeka02 <antonymbeka@gmail.com>
2023-04-17 16:22:40 +0300

    comment change

--- src/services/requests.js
+++ src/services/requests.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   baseURL: "http://localhost:3000",
 });
 
-//generic get request fn , pass endpoint to it
+//generic get request fn , pass  the req uri to it
 const getData = async (request) => {
   const response = await instance.get(request);
   return response.data;
